f90c9501c49e5912e2db5fbbf1ab4d7e8795d606
Aruanã Garcia <120034499+AruGarcia@users.noreply.github.com>
2023-05-23 19:19:42 -0300

    correção do exercício 18 da seção 01

--- secao_01_estrutura_sequencial/ex_18_tempo_de_download.py
+++ secao_01_estrutura_sequencial/ex_18_tempo_de_download.py
@@ -21,3 +21,16 @@ Arredonde o tempo em minutos
 
 def calcular_tempo_de_download():
     """Escreva aqui em baixo a sua solução"""
+
+    # 1 peça o tamanho de um arquivo para download (em MB)
+    tamanho_do_arquivo = int(input(f'Qual o tamanho do arquivo? '))
+    
+    # 2 velocidade de um link de Internet (em Mbps)
+    velocidade_da_internet = int(input(f'Qual a velocidade da internet? '))
+    tamanho_do_arquivo_em_Mb = tamanho_do_arquivo * 8
+    tempo_de_download_segundos = tamanho_do_arquivo_em_Mb / velocidade_da_internet 
+    tempo_de_download_minutos = round(tempo_de_download_segundos / 60)
+
+    # output
+    ##1 informe o tempo aproximado de download do arquivo (minutos), Arredonde o temp
+    print(f'Tempo aproximado do Download é: {tempo_de_download_minutos} minuto(s)')
