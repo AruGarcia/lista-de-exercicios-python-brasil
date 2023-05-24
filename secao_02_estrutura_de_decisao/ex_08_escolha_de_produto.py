22024ee1a90ebf3d00da834fa95ba19a5f3f89b6
Aruanã Garcia <120034499+AruGarcia@users.noreply.github.com>
2023-05-24 11:08:22 -0300

    correção do exercício 08 da seção 02

--- secao_02_estrutura_de_decisao/ex_08_escolha_de_produto.py
+++ secao_02_estrutura_de_decisao/ex_08_escolha_de_produto.py
@@ -20,3 +20,5 @@ Mostrar o resultado com duas casas decimais
 
 def decidir_melhor_produto(x, y, z):
     """Escreva aqui em baixo a sua solução"""
+    menor = min(x, y, z)
+    print(f'Melhor produto custa R$ {menor:.2f}')
