8d3283a02e7ba723d1f46cb7255c40999ec6ad58
Aruanã Garcia <120034499+AruGarcia@users.noreply.github.com>
2023-05-25 17:42:19 -0300

    correção do exercício 18 da seção 02

--- secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
+++ secao_02_estrutura_de_decisao/ex_18_validador_de_data.py
@@ -24,3 +24,19 @@ Faça um Programa que peça uma data no formato dd/mm/aaaa e determine se a mesm
 
 def validar_data(data: str):
     """Escreva aqui em baixo a sua solução"""
+    data_list = data.split("/")
+    tamanho_lista = len(data_list)
+
+    if tamanho_lista <= 1:
+        return 'Data inválida'
+    else:
+        dia = int(data_list[0])
+        mes = int(data_list[1])
+        ano = int(data_list[2]) 
+        if dia == 30 and mes == 2 and ano <= 9999:
+            return 'Data inválida'
+        elif dia <= 31 and mes <= 12 and ano <= 9999:
+            return 'Data válida'
+        else:
+            return 'Data inválida'
+    
