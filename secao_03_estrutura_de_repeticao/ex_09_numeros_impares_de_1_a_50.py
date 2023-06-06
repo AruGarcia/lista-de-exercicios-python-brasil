ec9f99f50d5c1b3a2c62310398cd4ff219c53739
Aruanã <aruanagarcia@gmail.com>
2023-06-06 14:35:20 -0300

    correção do exercício 09 da seção 03

--- secao_03_estrutura_de_repeticao/ex_09_numeros_impares_de_1_a_50.py
+++ secao_03_estrutura_de_repeticao/ex_09_numeros_impares_de_1_a_50.py
@@ -12,3 +12,9 @@ Faça um programa que imprima na tela apenas os números ímpares entre 1 e 50
 
 def calcular_numeros_impares_de_1_a_50() -> str:
     """Escreva aqui em baixo a sua solução"""
+    list_impar =[]
+    for num in range(1, 50, 2):
+        list_impar.append(num)
+
+    string_impar = ', '.join(map(str, list_impar))
+    return (f'{string_impar}')
